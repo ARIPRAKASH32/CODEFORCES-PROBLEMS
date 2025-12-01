3e96211c0ecab4384da578ff710a07a2242d9686
Ariprakash Nagaraj <ariprakash32@gmail.com>
2025-12-01 23:11:22 +0530

    CODEFORCES

---
+++ 2164A - Sequence Game.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        for (auto &x : a) cin >> x;
+        long long x;
+        cin >> x;
+        long long mn = *min_element(a.begin(), a.end());
+        long long mx = *max_element(a.begin(), a.end());
+        cout << (mn <= x && x <= mx ? "YES\n" : "NO\n");
+    }
+}
+
