d5a6a063e4654281fbaf74124f1999234789f5d2
Ariprakash N <ariprakash32@gmail.com>
2026-01-14 21:21:57 +0530

    Add files via upload

---
+++ 1907A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        char col = s[0];
+        char row = s[1];
+
+        for (char r = '1'; r <= '8'; r++) {
+            if (r != row) {
+                cout << col << r << "\n";
+            }
+        }
+
+        for (char c = 'a'; c <= 'h'; c++) {
+            if (c != col) {
+                cout << c << row << "\n";
+            }
+        }
+    }
+    return 0;
+}
